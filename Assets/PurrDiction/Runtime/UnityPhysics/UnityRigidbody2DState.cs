01ae37f8504e2f085c947e063909a514807809fa
BobsiUnity <martin.skovrup@gmail.com>
2025-01-25 13:47:44 +0100

    Predicted rigidbody 2d added

---
+++ Assets/PurrDiction/Runtime/UnityPhysics/UnityRigidbody2DState.cs
@@ -0,0 +1,17 @@
+using PurrNet.Prediction;
+using UnityEngine;
+
+namespace PurrDash
+{
+    public struct UnityRigidbody2DState : IPredictedData<UnityRigidbody2DState>
+    {
+        public Vector2 linearVelocity;
+        public float angularVelocity;
+        public float linearDamping;
+
+        public override string ToString()
+        {
+            return $"(linearVelocity: {linearVelocity}, angularVelocity: {angularVelocity})";
+        }
+    }
+}
