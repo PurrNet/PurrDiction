d61bda08d581a02cb2bd0d019148587e98958b95
BobsiUnity <martin.skovrup@gmail.com>
2025-02-04 16:11:41 +0100

    State machine update

--- Assets/PurrDiction/Runtime/StateMachine/TestNode.cs
+++ Assets/PurrDiction/Runtime/StateMachine/TestNode.cs
@@ -1,73 +1,31 @@
-using System;
 using FixMath.NET;
 using PurrNet.Prediction.StateMachine;
 using UnityEngine;
 
 namespace PurrNet.Prediction
 {
-    public class TestNode : PredictedStateNode<TestNodeInput, TestNodeData>
+    public class TestNode : PredictedStateNode<TestNode.TestNodeData>
     {
         public override void Enter()
         {
-            Debug.Log($"Entered state {gameObject.name}");
-        }
-
-        public override void Exit()
-        {
-            Debug.Log($"Exit state {gameObject.name}");
-        }
-
-        protected override TestNodeInput GetInput()
-        {
-            var input = new TestNodeInput()
-            {
-                isKeyPressed = Input.GetKey(KeyCode.X)
-            };
-            
-            return input;
+            //Happens within simulation
+            Debug.Log($"Entered state {gameObject.name}", machine);
         }
 
         public override void StateSimulate(Fix64 delta)
         {
-            base.StateSimulate(delta);
-
-            var state = currentState;
-            state.testValue += (float)delta;
-            currentState = state;
+            //Happens within simulation
         }
 
-        protected override void Simulate(TestNodeInput? input, ref TestNodeData state, Fix64 delta)
+        public override void Exit()
         {
-            if (!input.HasValue)
-                return;
+            //Happens within simulation
+            Debug.Log($"Exit state {gameObject.name}");
+        }
+
+        public struct TestNodeData : IPredictedData<TestNodeData>
+        {
             
-            if(state.wasKeyPressed != input.Value.isKeyPressed)
-            {
-                state.wasKeyPressed = input.Value.isKeyPressed;
-                if(state.wasKeyPressed)
-                    machine.Next();
-            }
         }
-    }
-
-    public struct TestData
-    {
-        public float testValue;
-
-        public override string ToString()
-        {
-            return testValue.ToString();
-        }
-    }
-    
-    public struct TestNodeData : IPredictedData<TestNodeData>
-    {
-        public bool wasKeyPressed;
-        public float testValue;
-    }
-
-    public struct TestNodeInput : IPredictedData
-    {
-        public bool isKeyPressed;
     }
 }
