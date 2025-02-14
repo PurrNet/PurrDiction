196066e746d684763c54bf78f16ecd5e3a5650ce
BlenMiner <blenminer123@gmail.com>
2025-02-14 02:10:06 +0100

    feat: PredictedRigidbody provides safe collision events

--- Assets/PurrDiction/Runtime/UnityPhysics/PredictedRigidbody.cs
+++ Assets/PurrDiction/Runtime/UnityPhysics/PredictedRigidbody.cs
@@ -7,8 +7,19 @@ namespace PurrNet.Prediction
     [AddComponentMenu("PurrDiction/Unity Rigidbody/Predicted Rigidbody")]
     public class PredictedRigidbody : PredictedIdentity<UnityRigidbodyState>
     {
+        public delegate void OnCollisionDelegate(Collision other);
+        public delegate void OnTriggerDelegate(Collider other);
+        
         [SerializeField] private Rigidbody _rigidbody;
-
+        
+        public event OnCollisionDelegate onCollisionEnter;
+        public event OnCollisionDelegate onCollisionExit;
+        public event OnCollisionDelegate onCollisionStay;
+        
+        public event OnTriggerDelegate onTriggerEnter;
+        public event OnTriggerDelegate onTriggerExit;
+        public event OnTriggerDelegate onTriggerStay;
+        
         private void Reset()
         {
             _rigidbody = GetComponent<Rigidbody>();
@@ -33,6 +44,54 @@ namespace PurrNet.Prediction
         {
             _rigidbody.linearVelocity = state.linearVelocity;
             _rigidbody.angularVelocity = state.angularVelocity;
+        }
+        
+        private void OnCollisionEnter(Collision other)
+        {
+            if (!predictionManager.isSimulating)
+                return;
+
+            onCollisionEnter?.Invoke(other);
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            if (!predictionManager.isSimulating)
+                return;
+
+            onCollisionExit?.Invoke(other);
+        }
+        
+        private void OnCollisionStay(Collision other)
+        {
+            if (!predictionManager.isSimulating)
+                return;
+
+            onCollisionStay?.Invoke(other);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!predictionManager.isSimulating)
+                return;
+            
+            onTriggerEnter?.Invoke(other);
+        }
+        
+        private void OnTriggerExit(Collider other)
+        {
+            if (!predictionManager.isSimulating)
+                return;
+            
+            onTriggerExit?.Invoke(other);
+        }
+        
+        private void OnTriggerStay(Collider other)
+        {
+            if (!predictionManager.isSimulating)
+                return;
+
+            onTriggerStay?.Invoke(other);
         }
     }
 }
