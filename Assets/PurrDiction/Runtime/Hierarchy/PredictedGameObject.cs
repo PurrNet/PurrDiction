4c280910ce1dd4c35cb7228ced37728367a1aee4
BlenMiner <blenminer123@gmail.com>
2025-07-20 22:14:51 +0200

    feat: PredictedGameObject for active state stuff

---
+++ Assets/PurrDiction/Runtime/Hierarchy/PredictedGameObject.cs
@@ -0,0 +1,27 @@
+namespace PurrNet.Prediction
+{
+    public struct PredictedGameObjectState : IPredictedData<PredictedGameObjectState>
+    {
+        public bool isActive;
+
+        public void Dispose() { }
+
+        public override string ToString()
+        {
+            return $"isActive={isActive}";
+        }
+    }
+
+    public class PredictedGameObject : PredictedIdentity<PredictedGameObjectState>
+    {
+        protected override void GetUnityState(ref PredictedGameObjectState state)
+        {
+            state.isActive = gameObject.activeSelf;
+        }
+
+        protected override void SetUnityState(PredictedGameObjectState state)
+        {
+            gameObject.SetActive(state.isActive);
+        }
+    }
+}
