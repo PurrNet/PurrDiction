f8704f06ef0464ce0c0c1d2b04831ed353829517
BobsiUnity <martin.skovrup@gmail.com>
2025-12-06 18:34:48 +0100

    Predicted module with state

---
+++ Assets/PurrDiction/Runtime/Core/PredictedModuleState.cs
@@ -0,0 +1,40 @@
+using PurrNet.Modules;
+using PurrNet.Packing;
+using UnityEngine;
+
+namespace PurrNet.Prediction
+{
+    public abstract class PredictedModule<TState> : PredictedModule where TState : struct, IPredictedData<TState>
+    {
+        protected History<TState> history { get; private set; }
+        public TState state;
+
+        protected override void OnSetup()
+        {
+            history = new History<TState>();
+        }
+
+        public override void Rollback(ulong tick)
+        {
+            if (history.Read(tick, out var result))
+            {
+                state = result;
+            }
+        }
+
+        public override void SaveState(ulong tick)
+        {
+            history.Write(tick, state);
+        }
+
+        public override void WriteState(BitPacker packer, DeltaModule deltaModule)
+        {
+            Packer<TState>.Write(packer, state);
+        }
+
+        public override void ReadState(BitPacker packer, DeltaModule deltaModule)
+        {
+            Packer<TState>.Read(packer, ref state);
+        }
+    }
+}
