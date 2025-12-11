9a503f8cc704c2df48f1d4191a884db7179923d2
BlenMiner <blenminer123@gmail.com>
2025-12-11 11:02:37 +0100

    not needed here

--- Assets/PurrDiction/Runtime/Core/PredictedIdentity.Module.cs
+++ Assets/PurrDiction/Runtime/Core/PredictedIdentity.Module.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 namespace PurrNet.Prediction
 {
-    public abstract partial class PredictedIdentity : MonoBehaviour
+    public abstract partial class PredictedIdentity
     {
         private readonly List<PredictedModule> _modules = new();
 
@@ -14,7 +14,7 @@ namespace PurrNet.Prediction
             for (int i = 0; i < _modules.Count; i++)
                 _modules[i].SetupInternal(this, world);
         }
-        
+
         public T RegisterModule<T>(T module) where T : PredictedModule
         {
             module.moduleIndex = _modules.Count;
@@ -54,14 +54,14 @@ namespace PurrNet.Prediction
             bool didWriteAny = false;
             for (int i = 0; i < _modules.Count; i++)
             {
-                didWriteAny |= _modules[i].WriteStateInternal(receiver, packer, deltaModule); 
+                didWriteAny |= _modules[i].WriteStateInternal(receiver, packer, deltaModule);
             }
             return didWriteAny;
         }
 
         protected void ReadModules(ulong tick, BitPacker packer, DeltaModule deltaModule)
         {
-            for (int i = 0; i < _modules.Count; i++) 
+            for (int i = 0; i < _modules.Count; i++)
             {
                 _modules[i].ReadStateInternal(tick, packer, deltaModule);
             }
@@ -76,26 +76,26 @@ namespace PurrNet.Prediction
         {
             for (int i = 0; i < _modules.Count; i++) _modules[i].ResetInterpolationInternal();
         }
-        
+
         protected void WriteFirstStateModules(ulong tick, BitPacker packer)
         {
-            for (int i = 0; i < _modules.Count; i++) 
+            for (int i = 0; i < _modules.Count; i++)
             {
-                _modules[i].WriteFirstStateInternal(tick, packer); 
+                _modules[i].WriteFirstStateInternal(tick, packer);
             }
         }
 
         protected void ReadFirstStateModules(ulong tick, BitPacker packer)
         {
-            for (int i = 0; i < _modules.Count; i++) 
+            for (int i = 0; i < _modules.Count; i++)
             {
-                _modules[i].ReadFirstStateInternal(tick, packer); 
+                _modules[i].ReadFirstStateInternal(tick, packer);
             }
         }
-        
+
         protected void ClearFutureModules(ulong tick)
         {
-            for (int i = 0; i < _modules.Count; i++) 
+            for (int i = 0; i < _modules.Count; i++)
             {
                 _modules[i].ClearFutureInternal(tick);
             }
